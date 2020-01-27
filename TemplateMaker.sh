44049bca0ab4fadec6cf9a77283058095ef03a2d
Sam de Jong <srdejong@uvic.ca>
2020-01-27 15:13:02 -0800

    TemplateMaker now defaults to date based filename

--- TemplateMaker.sh
+++ TemplateMaker.sh
@@ -1,15 +1,26 @@
 function SlideTemplate {
-   ln -s ~/Documents/Presentations/Template/preamble.tex ./preamble.tex
-   cp ~/Documents/Presentations/Template/Template.tex ./${1}.tex
-   cp ~/Documents/Presentations/Template/Template_slides.tex ./${1}_slides.tex
-   cp ~/Documents/Presentations/Template/Template_notes.tex ./${1}_notes.tex
-   cp ~/Documents/Presentations/Template/Template_handout.tex ./${1}_handout.tex
-   cp ~/Documents/Presentations/Template/Makefile ./
 
-   sed -i "s/Template/$1/g" ${1}_slides.tex
-   sed -i "s/Template/$1/g" ${1}_notes.tex
-   sed -i "s/Template/$1/g" ${1}_handout.tex
-   sed -i "s/Template/$1/g" Makefile
+    if [ -z "$1" ]; then
+	name=$(date +%y-%m-%d)
+    else
+	name=${1}
+    fi
+
+
+    mkdir ${name}
+    cd ${name}
+
+    ln -s ~/Documents/Presentations/Template/preamble.tex ./preamble.tex
+    cp ~/Documents/Presentations/Template/Template.tex ./${name}.tex
+    cp ~/Documents/Presentations/Template/Template_slides.tex ./${name}_slides.tex
+    cp ~/Documents/Presentations/Template/Template_notes.tex ./${name}_notes.tex
+    cp ~/Documents/Presentations/Template/Template_handout.tex ./${name}_handout.tex
+    cp ~/Documents/Presentations/Template/Makefile ./
+
+    sed -i "s/Template/$1/g" ${name}_slides.tex
+    sed -i "s/Template/$1/g" ${name}_notes.tex
+    sed -i "s/Template/$1/g" ${name}_handout.tex
+    sed -i "s/Template/$1/g" Makefile
 
 
 
